ee5ae1a83fc3b1d44ad05a2c0d03325f2388b95d
Celso Cardenas <ccardena@tibco.com>
2023-03-21 23:13:55 -0600

    primer commit, creación de proyecto y estructura básica

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,17 +1,8 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
 const SecondPage = () => (
-  <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  <h1>Hola Mundo 2</h1>
 )
-
-export const Head = () => <Seo title="Page two" />
 
 export default SecondPage
